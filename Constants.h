774ebdba1817435f89709f2b54974a4049a267b9
Pranav Kasetti <pranavkasetti@gmail.com>
2019-12-06 20:14:20 +0000

    Added constants.

---
+++ Constants.h
@@ -0,0 +1,10 @@
+# ifndef CONSTANTS_H
+# define CONSTANTS_H
+
+const char MIN_FILE = 'A';
+const char MAX_FILE = 'H';
+const char MIN_RANK = '1';
+const char MAX_RANK = '8';
+const int BOARD_DIMEN = 8;
+
+# endif
